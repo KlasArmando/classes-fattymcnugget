65462c9270c541a2621ff0a5bff450c2c9c6321c
fattymcnugget <bartpeeters2@hotmail.com>
2019-02-06 10:27:45 +0100

    added customer

--- index.php
+++ index.php
@@ -12,3 +12,19 @@ class Product{
 }
 $product = new Product("Pewdiepie chair",399);
 var_dump($product);
+
+class Customer{
+  public  $name;
+   public $age;
+   public $customerNumber;
+
+   public function  __construct($name,$age,$customerNumber)
+   {
+       $this->name=$name;
+       $this->age=$age;
+       $this->customerNumber=$customerNumber;
+   }
+}
+
+$customer= new Customer('Bart',17 , 83);
+var_dump($customer);
